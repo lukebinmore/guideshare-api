7b6de800cd16a64efa6ed5f854b0145fd647531b
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-05 07:00:38 +0300

    Update - Add Profile Admin Settings
    resolve #21

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "owner",
+        "first_name",
+        "last_name",
+        "dob",
+        "created_at",
+        "updated_at"
+    )
+    list_filter = (
+        "dob",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "__all__",
+    )
