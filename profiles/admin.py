c504f7ead041c4fdc2eeccdc07f317ae64674414
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-05 07:06:41 +0300

    Update - Add Redirect To Admin Panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = (
@@ -9,13 +10,11 @@ class ProfileAdmin(admin.ModelAdmin):
         "last_name",
         "dob",
         "created_at",
-        "updated_at"
+        "updated_at",
     )
     list_filter = (
         "dob",
         "created_at",
         "updated_at",
     )
-    search_fields = (
-        "__all__",
-    )
+    search_fields = ("__all__",)
