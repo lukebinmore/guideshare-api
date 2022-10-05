1289f47953f8ad54f506599747332401ab4f0dcf
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-05 06:20:18 +0300

    Update - Add Basic Profile App

---
+++ profiles/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+class Profile(models.Model):
+    def image_dir(self, filename):
+        return f"profiles/{self.owner.id}/{filename}"
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, blank=True)
+    last_name = models.CharField(max_length=25, blank=True)
+    dob = models.DateField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    picture = models.ImageField(
+        upload_to=image_dir,
+        default="/profiles/default/placeholder",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            owner=instance,
+        )
+
+
+post_save.connect(create_profile, sender=User)
