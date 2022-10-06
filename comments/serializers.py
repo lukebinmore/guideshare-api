bd3ba9ca2ef32661707f74f4da4beaf8b3db2970
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-07 02:19:08 +0300

    Update - Add Comment Serializer

---
+++ comments/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Comment
+from votes.models import Vote
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    likes_count = serializers.ReadOnlyField(default=0)
+    like_id = serializers.SerializerMethodField()
+    dislikes_count = serializers.ReadOnlyField(default=0)
+    dislike_id = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        return self.context["request"].user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Vote.objects.filter(owner=user, comment=obj, vote=0).first()
+        return like.id if like else None
+
+    def get_dislike_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            dislike = Vote.objects.filter(
+                owner=user, comment=obj, vote=1
+            ).first()
+        return dislike.id if dislike else None
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
