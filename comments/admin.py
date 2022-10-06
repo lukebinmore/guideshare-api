10691098a8ab0285973ef028c34817ba0c9453c6
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-07 01:26:17 +0300

    Change - Change Admin Config

--- comments/admin.py
+++ comments/admin.py
@@ -4,6 +4,12 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "owner",
+        "post",
+        "created_at",
+        "updated_at",
+    )
     list_filter = (
         "post",
         "created_at",
