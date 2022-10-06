34669bb9bcf93fa5f74c5df7ac9e221e7fee272b
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-07 00:42:46 +0300

    Update - Add Vote ID's & Comment Count to Post Serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,17 +1,36 @@
 from rest_framework import serializers
 from .models import Post
+from votes.models import Vote
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     likes_count = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
     dislikes_count = serializers.SerializerMethodField()
+    dislike_id = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
 
     def get_likes_count(self, obj):
         return obj.post_votes.filter(vote=0).count()
 
+    def get_like_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Vote.objects.filter(owner=user, post=obj, vote=0).first()
+        return like.id if like else None
+
     def get_dislikes_count(self, obj):
         return obj.post_votes.filter(vote=1).count()
+
+    def get_dislike_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            dislike = Vote.objects.filter(owner=user, post=obj, vote=1).first()
+        return dislike.id if dislike else None
+
+    def get_comments_count(self, obj):
+        return obj.post_comments.count()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- votes/urls.py
+++ votes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("votes/", views.VoteCreate.as_view()),
+    path("vote/<int:pk>/", views.VoteRetrieveDestroy.as_view()),
 ]
