d10952beed331d61caf607193a2fe3a10e203994
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-07 05:22:14 +0300

    Update - Enable Cloudinary Static File Storage

--- guideshareapi/settings.py
+++ guideshareapi/settings.py
@@ -178,7 +178,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/guideshare/static/"
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
