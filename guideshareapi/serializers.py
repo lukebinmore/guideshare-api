af26e270c8a4057cdbcc3f33d305d5bf5c499316
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-13 15:03:32 +0300

    Bug Fix - Missed Name Change from Previous Bug Fix
    Issue:
    Field name was updated in retuned fields, but not the declared name.
    
    Resolution:
    Updated the declared field name.

--- guideshareapi/serializers.py
+++ guideshareapi/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source="profile.id")
-    profile_image = serializers.ReadOnlyField(source="profile.picture.url")
+    profile_picture = serializers.ReadOnlyField(source="profile.picture.url")
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
