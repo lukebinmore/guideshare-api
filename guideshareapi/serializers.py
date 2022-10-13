20d7618ce4827349d8c50d8a3fab3b6187d02bcb
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-13 14:46:38 +0300

    Bug Fix - Incorrect User Details Serializer Source Values & Names
    Issue:
    Incorrect use of underscores instead of dot notation and incorrect field names in guideshareapi serializers.
    
    Resolution:
    Replaced underscores with correct dot notation and changed used field names.

--- guideshareapi/serializers.py
+++ guideshareapi/serializers.py
@@ -3,11 +3,11 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source="profile_id")
-    profile_image = serializers.ReadOnlyField(source="profile_image")
+    profile_id = serializers.ReadOnlyField(source="profile.id")
+    profile_image = serializers.ReadOnlyField(source="profile.picture.url")
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             "profile_id",
-            "profile_image",
+            "profile_picture",
         )
