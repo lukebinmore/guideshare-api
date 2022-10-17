121e0442c3f1848fe1aa108ebae728624c2c3cba
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-17 18:17:01 +0300

    Update - Add Is_Admin Field to User Serializer

--- guideshareapi/serializers.py
+++ guideshareapi/serializers.py
@@ -5,9 +5,13 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source="profile.id")
     profile_picture = serializers.ReadOnlyField(source="profile.picture.url")
+    is_admin = serializers.BooleanField(
+        source="is_staff", read_only=True, default=False
+    )
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             "profile_id",
             "profile_picture",
+            "is_admin",
         )
