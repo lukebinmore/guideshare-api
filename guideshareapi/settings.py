222f1c597d0dbabee9a0be397133b0ee0348b693
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-19 00:35:53 +0300

    Change - Change Default Pagination Quantity

--- guideshareapi/settings.py
+++ guideshareapi/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
         )
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 9,
+    "PAGE_SIZE": 20,
     "DATETIME_FORMAT": "%d %b %Y, %H:%M",
 }
 
