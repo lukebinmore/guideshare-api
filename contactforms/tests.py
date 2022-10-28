d0841f63c9b8d1b942133674cb04a27e6ef7106d
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-28 04:02:25 +0300

    Update - Add ContactForm App Unit Tests

--- contactforms/tests.py
+++ contactforms/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class ContactFormViewTest(APITestCase):
+    def test_can_submit_valid_form(self):
+        response = self.client.post(
+            "/contact-form/",
+            {
+                "title": "Contact Form Title",
+                "email": "peter@email.com",
+                "reason": 1,
+                "content": "Contact Form Content",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cant_submit_invalid_form(self):
+        response = self.client.post(
+            "/contact-form/",
+            {
+                "email": "peter@email.com",
+                "reason": 8,
+                "content": "Contact Form Content",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
