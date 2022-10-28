ed67b0f05413c52d3404958e5b064200227da1dc
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-28 04:02:56 +0300

    Update - Add Votes App Unit Tests

--- votes/tests.py
+++ votes/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Vote
+from posts.models import Post, Category
+from comments.models import Comment
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class VoteCreateViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username="peter", password="pass")
+        peter = User.objects.get(username="peter")
+        Category.objects.create(title="Category Title")
+        category = Category.objects.get(id=1)
+        Post.objects.create(
+            id=1, owner=peter, category=category, content="Post Content"
+        )
+        Post.objects.create(
+            id=2, owner=peter, category=category, content="Second Post Content"
+        )
+        post = Post.objects.get(id=1)
+        Comment.objects.create(
+            id=1, owner=peter, post=post, content="Comment Content"
+        )
+        Vote.objects.create(id=1, owner=peter, post=post, vote=0)
+
+    def test_users_can_create_post_votes(self):
+        self.client.login(username="peter", password="pass")
+        response = self.client.post(
+            "/votes/", {"post": 2, "comment": "", "vote": 0}
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_users_cant_create_diplicate_votes(self):
+        self.client.login(username="peter", password="pass")
+        response = self.client.post(
+            "/votes/", {"post": 1, "comment": "", "vote": 0}
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_users_can_create_comment_votes(self):
+        self.client.login(username="peter", password="pass")
+        response = self.client.post(
+            "/votes/", {"post": "", "comment": 1, "vote": 0}
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class VoteDestroyViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username="peter", password="pass")
+        peter = User.objects.get(username="peter")
+        Category.objects.create(title="Category Title")
+        category = Category.objects.get(id=1)
+        Post.objects.create(
+            id=1, owner=peter, category=category, content="Post Content"
+        )
+        post = Post.objects.get(id=1)
+        Comment.objects.create(
+            id=1, owner=peter, post=post, content="Comment Content"
+        )
+        Vote.objects.create(id=1, owner=peter, post=post, vote=0)
+
+    def test_users_can_delete_own_votes(self):
+        self.client.login(username="peter", password="pass")
+        response = self.client.delete("/votes/1/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_users_cant_delete_others_votes(self):
+        self.client.login(username="steve", password="pass")
+        response = self.client.delete("/votes/1/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
